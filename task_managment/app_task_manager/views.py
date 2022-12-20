820fa70713fb49fecce7911a012a82a7cc310148
aecf <81099969+albertoecf@users.noreply.github.com>
2022-12-20 08:06:09 +0100

    feat: form connected with db and fe

--- task_managment/app_task_manager/views.py
+++ task_managment/app_task_manager/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import taskModel
+from .forms import taskForm
+from django.contrib import messages
 
-def home(requests):
 
-    all_items = taskModel.objects.all()
+def home(request):
 
-    return render(requests, 'index.html', {'all_items':all_items})
+    if request.method == 'POST':
+        form = taskForm(request.POST or None)
+        if form.is_valid():
+            print('form valid')
+            form.save()
+            all_items = taskModel.objects.all()
+            messages.success(request, ("New item addedd.."))
+            print(all_items)
+            return render(request, 'index.html', {'all_items': all_items})
+        else :
+            print('invalid form')
+            return render(request, 'index.html', {'all_items': 'Not valid form'})
+    else:
+        all_items = taskModel.objects.all()
+        return render(request, 'index.html', {'all_items': all_items})
